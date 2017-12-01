cc6b9afa0bb05e3c8d48169737ff37e45ef9cddb
jasonduquain <jduquain@gmail.com>
2017-12-01 08:18:51 -0500

    starting files in place

---
+++ gulpfile - COMPLETE.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass'); 
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+//var breakpoint = require('breakpoint-sass');
+
+
+
+/* added autoprefixer before sourcemaps.write() to fix errors related to double slashes in scss files) */
+
+gulp.task('styles', function() {
+    gulp.src('scss/**/*scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'expanded'
+    })).on('error', sass.logError)
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', ['styles'], function() {
+    gulp.watch('scss/**/*scss', ['styles']);
+});
+
+gulp.task('default', ['watch']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
